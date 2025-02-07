274cce4028543bf3ee1010b7654bdb73db53b350
hz-uiw <kwonmc5.11@gmail.com>
2025-02-07 15:20:41 +0900

    book 작업 중

--- src/main/java/com/korit/springboot_study/dto/request/ReqAddBookDto.java
+++ src/main/java/com/korit/springboot_study/dto/request/ReqAddBookDto.java
@@ -2,13 +2,19 @@ package com.korit.springboot_study.dto.request;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
-@NoArgsConstructor
 public class ReqAddBookDto {
-    @ApiModelProperty
+    @ApiModelProperty(value = "도서명", example = "소나무", required = true)
     private String bookName;
+    @ApiModelProperty(value = "저자 ID", example = "123456", required = true)
+    private int authorId;
+    @ApiModelProperty(value = "ISBN", example = "C12345", required = true)
+    private String isbn;
+    @ApiModelProperty(value = "카테고리 ID", example = "123456", required = true)
+    private int categoryId;
+    @ApiModelProperty(value = "출판사 ID", example = "123456", required = true)
+    private int publisherId;
+    @ApiModelProperty(value = "이미지 경로(URL)", example = "http://example.com/test.jpg", required = true)
+    private String bookImgUrl;
 }
--- src/main/java/com/korit/springboot_study/dto/request/ReqAddPublisher.java
+++ src/main/java/com/korit/springboot_study/dto/request/ReqAddPublisher.java
@@ -2,12 +2,8 @@ package com.korit.springboot_study.dto.request;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
-@NoArgsConstructor
 public class ReqAddPublisher {
     @ApiModelProperty(value = "출판사명", example = "글나무", required = true)
     private String publisherName;
