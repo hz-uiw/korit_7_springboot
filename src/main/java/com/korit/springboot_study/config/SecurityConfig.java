fc40bd041c8cf73d13151ece9a2121b0002feda2
hz-uiw <kwonmc5.11@gmail.com>
2025-02-17 10:52:04 +0900

    HealthCheck

--- src/main/java/com/korit/springboot_study/config/SecurityConfig.java
+++ src/main/java/com/korit/springboot_study/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         http.csrf().disable();
         http.httpBasic().disable();
         http.formLogin().disable();
@@ -61,7 +62,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/swagger-ui/**",
                         "/v2/api-docs/**",
                         "/v3/api-docs/**",
-                        "/swagger-resources/**"
+                        "/swagger-resources/**",
+                        "/actuator/health",
+                        "/server/hc"
                 )
                 .permitAll()
                 .antMatchers(
